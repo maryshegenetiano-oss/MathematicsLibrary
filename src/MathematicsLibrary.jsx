c83ad4062f250f2ecc3b0b1c4435b665a0769d78
maryshegenetiano-oss <maryshegenetiano@gmail.com>
2026-02-24 19:11:20 +0800

    Move MathematicsLibrary.jsx to src folder

---
+++ src/MathematicsLibrary.jsx
@@ -0,0 +1,14 @@
+// Assuming the content of MathematicsLibrary.jsx is the following:
+
+import React from 'react';
+
+const MathematicsLibrary = () => {
+    return (
+        <div>
+            <h1>Mathematics Library</h1>
+            {/* Add your mathematical functions here */}
+        </div>
+    );
+};
+
+export default MathematicsLibrary;
